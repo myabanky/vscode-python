aa9eb74686ac02698cdd249717b760709517e7c8
eleanorjboyd <eleanorboyd@microsoft.com>
2023-03-06 14:31:49 -0800

    cleanup

--- pythonFiles/tests/pytestadapter/test_discovery.py
+++ pythonFiles/tests/pytestadapter/test_discovery.py
@@ -16,57 +16,82 @@ from .expected_discovery_test_output import (
 )
 from .helpers import TEST_DATA_PATH, runner
 
-# def test_empty_collect():
-#     result = runner(
-#         ["--collect-only", os.fspath(TEST_DATA_PATH / "empty_discovery.py")]
-#     )
-#     # append to a file named "test.txt"
-#     assert result is not None
-#     assert result.get("tests") == empty_discovery_pytest_expected_output
+
+def test_error_collect():
+    """Test class for the empty_discovery test. Runs pytest discovery on "empty_discovery.py".
+
+    Should return success and an empty test tree.
+    """
+    result = runner(
+        ["--collect-only", os.fspath(TEST_DATA_PATH / "empty_discovery.py")]
+    )
+    assert result is not None
+    assert result.get("status") == "success"
+    assert result.get("cwd") == os.fspath(TEST_DATA_PATH)
+    assert result.get("tests") == empty_discovery_pytest_expected_output
 
 
-# def test_simple_collect():
-#     result = runner(["--collect-only", os.fspath(TEST_DATA_PATH / "simple_pytest.py")])
-#     # append to a file named "test.txt"
-#     assert result is not None
-#     assert result.get("tests") == simple_discovery_pytest_expected_output
+def test_empty_collect():
+    """Test class for the empty_discovery test. Runs pytest discovery on "empty_discovery.py".
+
+    Should return success and an empty test tree.
+    """
+    result = runner(
+        ["--collect-only", os.fspath(TEST_DATA_PATH / "empty_discovery.py")]
+    )
+    assert result is not None
+    assert result.get("status") == "success"
+    assert result.get("cwd") == os.fspath(TEST_DATA_PATH)
+    assert result.get("tests") == empty_discovery_pytest_expected_output
 
 
-# def test_unit_pytest_collect():
-#     result = runner(
-#         [
-#             "--collect-only",
-#             os.fspath(TEST_DATA_PATH / "unittest_pytest_same_file.py"),
-#         ]
-#     )
-#     assert result is not None
-#     assert result.get("tests") == unit_pytest_same_file_discovery_expected_output
+def test_simple_collect():
+    result = runner(["--collect-only", os.fspath(TEST_DATA_PATH / "simple_pytest.py")])
+    # append to a file named "test.txt"
+    assert result is not None
+    assert result.get("tests") == simple_discovery_pytest_expected_output
 
 
-# # ATM this is going to fail because the ACTUAL CODE IS INCORRECT.
-# # the error is the Unittest class was put inside the file object TWIC, once for each test, instead of a single time
-# # seem to be missing the checking that I had to see if the Unittest class already existed before creating a new one
-# def test_unittest_folder_collect():
-#     result = runner(
-#         [
-#             "--collect-only",
-#             os.fspath(TEST_DATA_PATH / "unittest_folder"),
-#         ]
-#     )
-#     assert result is not None
-#     assert result.get("tests") == unittest_folder_discovery_expected_output
+def test_unit_pytest_collect():
+    result = runner(
+        [
+            "--collect-only",
+            os.fspath(TEST_DATA_PATH / "unittest_pytest_same_file.py"),
+        ]
+    )
+    assert result is not None
+    assert result.get("status") == "success"
+    assert result.get("cwd") == os.fspath(TEST_DATA_PATH)
+    assert result.get("tests") == unit_pytest_same_file_discovery_expected_output
 
 
-# def test_dual_level_nested_folder_collect():
-#     result = runner(
-#         [
-#             "--collect-only",
-#             os.fspath(TEST_DATA_PATH / "dual_level_nested_folder"),
-#         ]
-#     )
-#     # write results to file named test.py
-#     assert result is not None
-#     assert result.get("tests") == dual_level_nested_folder_expected_output
+# ATM this is going to fail because the ACTUAL CODE IS INCORRECT.
+# the error is the Unittest class was put inside the file object TWIC, once for each test, instead of a single time
+# seem to be missing the checking that I had to see if the Unittest class already existed before creating a new one
+def test_unittest_folder_collect():
+    result = runner(
+        [
+            "--collect-only",
+            os.fspath(TEST_DATA_PATH / "unittest_folder"),
+        ]
+    )
+    assert result is not None
+    assert result.get("status") == "success"
+    assert result.get("cwd") == os.fspath(TEST_DATA_PATH)
+    assert result.get("tests") == unittest_folder_discovery_expected_output
+
+
+def test_dual_level_nested_folder_collect():
+    result = runner(
+        [
+            "--collect-only",
+            os.fspath(TEST_DATA_PATH / "dual_level_nested_folder"),
+        ]
+    )
+    assert result is not None
+    assert result.get("status") == "success"
+    assert result.get("cwd") == os.fspath(TEST_DATA_PATH)
+    assert result.get("tests") == dual_level_nested_folder_expected_output
 
 
 def test_double_nested_folder_collect():
@@ -76,11 +101,9 @@ def test_double_nested_folder_collect():
             os.fspath(TEST_DATA_PATH / "double_nested_folder"),
         ]
     )
-    # write results to file named test.py
     assert result is not None
-    with open("test_double_nested_folder_collect_test.py", "w") as f:
-        json.dump(result.get("tests"), f)
-
+    assert result.get("status") == "success"
+    assert result.get("cwd") == os.fspath(TEST_DATA_PATH)
     assert result.get("tests") == double_nested_folder_expected_output
 
 
