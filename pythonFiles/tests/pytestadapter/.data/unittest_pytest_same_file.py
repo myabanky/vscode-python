95c408b6e861738e95cf98bdf67e209cce0fd66c
eleanorjboyd <eleanorboyd@microsoft.com>
2023-03-09 15:39:47 -0800

    create find line no function

--- pythonFiles/tests/pytestadapter/.data/unittest_pytest_same_file.py
+++ pythonFiles/tests/pytestadapter/.data/unittest_pytest_same_file.py
@@ -10,6 +10,7 @@ class TestExample(unittest.TestCase):
     def test_true_unittest(self):  # test_marker--test_true_unittest
         assert True
 
+
 # This test's id is unittest_pytest_same_file.py::test_true_pytest.
 # Test type is pytest and this test passes.
 def test_true_pytest():  # test_marker--test_true_pytest
